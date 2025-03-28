e0b9d2f0899e6639e00c860c75c09d26ecf17abb
Naimur <naimur30447@gmail.com>
2025-03-29 02:02:04 +0600

    Question form

--- client/src/app/storage/storage.tsx
+++ client/src/app/storage/storage.tsx
@@ -1,68 +1,67 @@
 import { QuestionType } from "@/lib/type";
 
 export const allQuestions: QuestionType[] = [
-  {
-    _id: "8e9dh829382",
-    class: "HSC",
-    subject: "Physics",
-    paper: 1,
-    chapter: 10,
-    topic: "Gravity",
-    questionType: "mcq",
-    standard: "board",
-    record: [
-      ["dhaka", "24", "5"],
-      ["rajshahi", "24", "1"],
-    ],
-    tag: ["Engr_Model_Test_24", "Engr_Model_Test_24"],
-    detail: [
-      "Why do objects fall down lorme hello how are you. Ima doing well how are you So What re you doin now are you. Ima doing well how are you So What re you doin now?",
-      "1",
-    ],
-    options: [
-      "magnetic force",
-      "gravitational force",
-      "friction",
-      "internal energy",
-    ],
-    answer: "gravitational force",
-    mark: 1,
-    toughness: 5,
-    explanation:
-      "Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you. ",
-    time: 1,
-  },
-  {
-    _id: "32938dhd39",
-    class: "HSC",
-    subject: "Physics",
-    paper: 1,
-    chapter: 10,
-    topic: "Gravity",
-    questionType: "mcq",
-    standard: "board",
-    record: [
-      ["dhaka", "24", "5"],
-      ["rajshahi", "24", "1"],
-    ],
-    tag: ["Engr_Model_Test_24", "Engr_Model_Test_24"],
-    detail: [
-      "Why do objects fall down lorme hello how are you. Ima doing well how are you So What re you doin now are you. Ima doing well how are you So What re you doin now?",
-      "1",
-    ],
-    options: [
-      "magnetic force",
-      "gravitational force",
-      "friction",
-      "internal energy",
-    ],
-    answer: "gravitational force",
-    mark: 1,
-    toughness: 5,
-    explanation:
-      "Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you. ",
-    time: 1,
-  },
+  // {
+  //   _id: "8e9dh829382",
+  //   class: "HSC",
+  //   subject: "Physics",
+  //   paper: 1,
+  //   chapter: 10,
+  //   topic: "Gravity",
+  //   questionType: "mcq",
+  //   standard: "board",
+  //   record: [
+  //     ["dhaka", "24", "5"],
+  //     ["rajshahi", "24", "1"],
+  //   ],
+  //   detail: [
+  //     "Why do objects fall down lorme hello how are you. Ima doing well how are you So What re you doin now are you. Ima doing well how are you So What re you doin now?",
+  //     "1",
+  //   ],
+  //   options: [
+  //     "magnetic force",
+  //     "gravitational force",
+  //     "friction",
+  //     "internal energy",
+  //   ],
+  //   answer: "gravitational force",
+  //   mark: 1,
+  //   toughness: 5,
+  //   explanation:
+  //     "Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you. ",
+  //   time: 1,
+  // },
+  // {
+  //   _id: "32938dhd39",
+  //   class: "HSC",
+  //   subject: "Physics",
+  //   paper: 1,
+  //   chapter: 10,
+  //   topic: "Gravity",
+  //   questionType: "mcq",
+  //   standard: "board",
+  //   record: [
+  //     ["dhaka", "24", "5"],
+  //     ["rajshahi", "24", "1"],
+  //   ],
+  //   tag: ["Engr_Model_Test_24", "Engr_Model_Test_24"],
+  //   detail: [
+  //     "Why do objects fall down lorme hello how are you. Ima doing well how are you So What re you doin now are you. Ima doing well how are you So What re you doin now?",
+  //     "1",
+  //   ],
+  //   options: [
+  //     "magnetic force",
+  //     "gravitational force",
+  //     "friction",
+  //     "internal energy",
+  //   ],
+  //   answer: "gravitational force",
+  //   mark: 1,
+  //   toughness: 5,
+  //   explanation:
+  //     "Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you. ",
+  //   time: 1,
+  // },
   //   {
   //     class: "HSC",
   //     subject: "Physics",
@@ -76,7 +75,6 @@ export const allQuestions: QuestionType[] = [
   //     statement:
   //       "Why do objects fall down lorme hello how are you. Ima doing well how are you So What re you doin now are you. Ima doing well how are you So What re you doin now?",
   //     detail: ["what is programming?", "1", "1"],
-
   //     answer:
   //       "Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you. Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you. Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you.Allah asehi banaiya hee vai. Tu samajta q nehi hee. Na bujhle, Mukhosto kor.are you.",
   //     toughness: 5,
