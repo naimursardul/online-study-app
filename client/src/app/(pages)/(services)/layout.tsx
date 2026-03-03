e4e790e883d262f2c2ed0751cd29a3f1cb88dcb1
Naimur <naimur30447@gmail.com>
2026-03-03 23:28:18 +0600

    come back

--- client/src/app/(pages)/(services)/layout.tsx
+++ client/src/app/(pages)/(services)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import ServiceNavbar from "@/components/service-navbar";
