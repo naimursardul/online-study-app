ff751182dad28eaf30448c73351baa10d003e0f7
Naimur <naimur30447@gmail.com>
2026-03-07 19:50:34 +0600

    Texteditor created

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -8,13 +8,13 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (
-    request.nextUrl.pathname.startsWith("/admin") &&
-    // (!user || user.role !== "admin")
-    !user
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // if (
+  //   request.nextUrl.pathname.startsWith("/admin") &&
+  //   // (!user || user.role !== "admin")
+  //   !user
+  // ) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 }
 
 export const config = {
